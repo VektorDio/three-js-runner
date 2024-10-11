f1b55b00841ad39bd05da60ed43e2905dd29d0f2
Nyan <vektordiod@gmail.com>
2024-10-11 12:42:31 +0300

    set up base project

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import './style.css'
+import * as THREE from 'three';
+
+// Initialize Three.js
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(2, 2, 4);
+camera.lookAt(0, 0, 0);
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Render the scene
+renderer.render(scene, camera);
